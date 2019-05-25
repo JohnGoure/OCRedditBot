1c1a01500e6028623ab525f463e96ef94f0735e4
John Goure <johngoure@protonmail.com>
2019-05-25 17:28:38 -0500

    Implemented a reader.

--- bot_read.py
+++ bot_read.py
@@ -2,10 +2,10 @@ import praw
 
 reddit = praw.Reddit('bot')
 
-subreddit = reddit.subreddit("learnpython")
+subreddit = reddit.subreddit('learnpython')
 
 for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("------------------------------------\n")
+    print('Title: ', submission.title)
+    print('Text: ', submission.selftext)
+    print('Score: ', submission.score)
+    print('------------------------------------\n')
