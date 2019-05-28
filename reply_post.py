579b071be61ff8ed0344ffa3847525b279d8ea08
John Goure <johngoure@protonmail.com>
2019-05-27 21:45:04 -0500

    Added a description check.

--- reply_post.py
+++ reply_post.py
@@ -16,13 +16,13 @@ else:
 subreddit = reddit.subreddit('pythonforengineers')
 for submission in subreddit.hot(limit=5):
     if submission.id not in posts_replied_to:
-        if re.search("i love python", submission.title, re.IGNORECASE):
+        if re.search("i love python", submission.title, re.IGNORECASE) or re.search('i love python', submission.description, re.IGNORECASE):
             submission.reply("Me TOO!!")
             posts_replied_to.append(submission.id)
             with open('posts_replied_to.txt', 'w') as f:
                 for post_id in posts_replied_to:
                     f.write(post_id + '\n')
-        elif re.search("test", submission.title, re.IGNORECASE):
+        elif re.search("test", submission.title, re.IGNORECASE) or re.search('test', submission.description, re.IGNORECASE):
             submission.reply('What are we testing?')
             posts_replied_to.append(submission.id)
             with open('posts_replied_to.txt', 'w') as f:
