5758b138c24eadaed49d89fba4f83029a9c9e264
John Goure <johngoure@protonmail.com>
2019-05-27 22:08:01 -0500

    Added code to read comments and track read comments.

--- reply_post.py
+++ reply_post.py
@@ -13,20 +13,35 @@ else:
         posts_replied_to = posts_replied_to.split('\n')
         posts_replied_to = list(filter(None, posts_replied_to))
 
+if not os.path.isfile('comments_read.txt'):
+    comments_read = []
+else:
+    with open('comments_read.txt') as f:
+        comments_read = f.read()
+        comments_read = comments_read.split('\n')
+        comments_read = list(filter(None, comments_read))
+
 subreddit = reddit.subreddit('pythonforengineers')
-for submission in subreddit.hot(limit=5):
+for submission in subreddit.hot(limit=10):
     if submission.id not in posts_replied_to:
-        if re.search("i love python", submission.title, re.IGNORECASE) or re.search('i love python', submission.description, re.IGNORECASE):
+        if re.search("i love python", submission.title, re.IGNORECASE) or re.search('i love python', submission.selftext, re.IGNORECASE):
             submission.reply("Me TOO!!")
             posts_replied_to.append(submission.id)
             with open('posts_replied_to.txt', 'w') as f:
                 for post_id in posts_replied_to:
                     f.write(post_id + '\n')
-        elif re.search("test", submission.title, re.IGNORECASE) or re.search('test', submission.description, re.IGNORECASE):
+        elif re.search("test", submission.title, re.IGNORECASE) or re.search('test', submission.selftext, re.IGNORECASE):
             submission.reply('What are we testing?')
             posts_replied_to.append(submission.id)
             with open('posts_replied_to.txt', 'w') as f:
                 for post_id in posts_replied_to:
                     f.write(post_id + '\n')
-
-print(f'Connected to sub-reddit {reddit.subreddit.displayname}.')
+        submission.comments.replace_more(limit=0)
+        for comment in submission.comments.list():
+            if comment.id not in comments_read:
+                print(comment.body)
+                comments_read.append(comment.id)
+            
+with open('comments_read.txt', 'w') as f:
+    for comment_id in comments_read:
+        f.write(comment_id + '\n')
